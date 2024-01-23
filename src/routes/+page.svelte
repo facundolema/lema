eaccdb00039ba55f2e9bb69697f1781a69a1fc0f
FacundoLema <facurlema@gmail.com>
2024-01-23 00:26:00 -0300

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <main>
   <div class="logo-container">
-    <img src="/logo.png" alt="logo"/>
+    <img src="/logo.png" alt="logo" />
   </div>
   <nav class="text-style-1">
     <span class="active">El estudio</span>
@@ -13,25 +13,56 @@
   <section class="presentation">
     <h1 class="text-style-1">Qué hacemos</h1>
     <p class="text-style-2">
-      Somos una firma legal dedicada exclusivamente al asesoramiento y al litigio tributario. Estamos enfocados en brindar nuestro servicio profesional personalizado, con exigentes estándares de calidad, lo que nos permite alcanzar excelentes resultados y altos niveles de satisfacción de nuestros clientes. La tributación nacional, provincial y municipal, en sus diversas expresiones, nos ocupan cotidianamente, laboral y académicamente. 
+      Somos una firma legal dedicada exclusivamente al asesoramiento y al
+      litigio tributario. Estamos enfocados en brindar nuestro servicio
+      profesional personalizado, con exigentes estándares de calidad, lo que nos
+      permite alcanzar excelentes resultados y altos niveles de satisfacción de
+      nuestros clientes. La tributación nacional, provincial y municipal, en sus
+      diversas expresiones, nos ocupan cotidianamente, laboral y académicamente.
     </p>
-    <img src="/pic01.jpg" alt="pic01"/>
+    <img src="/pic01.jpg" alt="pic01" />
   </section>
   <section class="socios">
     <h1 class="text-style-1">Socios</h1>
     <div class="divider">
       <div class="text-style-2">
         <h2 class="text-style-1">Enrique Bulit Goñi</h2>
-        <img src="/Bulit.jpg" alt="Bulit"/>
+        <img src="/Bulit.jpg" alt="Bulit" />
         <a href="/socios#enrique-bulit-goni">Más información</a>
       </div>
       <div class="text-style-2">
         <h2 class="text-style-1">Rodrigo Lema</h2>
-        <img src="/Lema.jpg" alt="Lema"/>
+        <img src="/Lema.jpg" alt="Lema" />
         <a href="/socios#rodrigo-lema">Más información</a>
       </div>
     </div>
   </section>
+  <section class="contacto">
+    <h1 class="text-style-1">Contacto</h1>
+    <div class="divider">
+      <div class="text-style-2">
+        <form class="text-style-1">
+          <label for="name">Nombre completo</label>
+          <input type="text" id="name" />
+          <label for="email">Correo electrónico</label>
+          <input type="email" id="email" />
+          <label for="message">Mensaje</label>
+          <textarea id="message"/>
+          <button type="submit">Enviar mensaje</button>
+        </form>
+      </div>
+      <div class="text-style-1 contact-info">
+        <div>Direccion</div>
+        <div>Telefono</div>
+        <div>Email</div>
+      </div>
+    </div>
+  </section>
+  <footer>
+    <div class="text-style-1">
+      <div>© 2024 BULIT GOÑI | LEMA abogados</div>
+    </div>
+  </footer>
 </main>
 
 <style>
@@ -41,8 +72,8 @@
     align-items: center;
     justify-content: flex-start;
     height: 100vh;
-    /* margin: 6rem; */
-    max-width: 72rem;
+    max-width: 72em;
+    width: calc(100% - 4rem);
     margin: auto;
     box-sizing: border-box;
   }
@@ -61,7 +92,9 @@
     flex-grow: 1;
     text-align: center;
     color: white;
-    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+    transition:
+      background-color 0.2s ease-in-out,
+      color 0.2s ease-in-out;
   }
 
   nav > span.active {
@@ -74,15 +107,22 @@
   }
 
   img {
-        width: 100%;
-        height: auto;
+    width: 100%;
+    height: auto;
   }
 
   h1 {
     font-size: 4rem;
   }
-  
-  section.presentation {
+
+  h2 {
+    text-align: center;
+    font-size: 1.75rem;
+    line-height: 1.3rem;
+    margin-bottom: 1.5rem;
+  }
+
+  section {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -90,33 +130,31 @@
     flex-grow: 1;
     width: 100%;
     background-color: white;
-    padding: 6rem 8rem;
     box-sizing: border-box;
-    & p {
-      font-style: italic;
-      text-align: center;
-    };
+  }
+
+  section.presentation > p {
+    padding: 6rem 8rem;
+    font-style: italic;
+    text-align: center;
   }
 
   section.socios {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    flex-grow: 1;
-    width: 100%;
-    background-color: white;
+    & img {
+      margin: 2.5rem 0;
+    }
   }
 
-  section.socios > div.divider {
+  div.divider {
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
     width: 100%;
     box-sizing: border-box;
-    background-color: white;
-    & div {
+    height: max-content;
+    border: 2px solid rgb(200, 200, 200);
+    & > div {
       display: flex;
       flex-direction: column;
       align-items: center;
@@ -125,10 +163,7 @@
       box-sizing: border-box;
       padding: 4rem;
       border: 1px solid rgb(200, 200, 200);
-      & h2 {
-        text-align: center;
-        text-transform: uppercase;
-      };
+
       & a {
         color: #212931;
         text-decoration: none;
@@ -136,8 +171,74 @@
       }
     }
   }
+
   div.logo-container {
     padding: 4rem;
     margin-bottom: 6rem;
+  }
+
+  section.contacto {
+    background-color: #dddddd;
+  }
+  
+  form {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    justify-content: center;
+    color: rgb(113, 121, 129);
+    width: 100%;
+    box-sizing: border-box;
+    font-size: 0.8rem;
+    & input {
+      width: 100%;
+      margin: 0.5rem 0;
+      padding: 0.5rem;
+      height: 2rem;
+      border: 2px solid rgb(200, 200, 200);
+      background-color: transparent;
+    }
+    & textarea {
+      width: 100%;
+      margin: 0.5rem 0;
+      padding: 0.5rem;
+      border: 2px solid rgb(200, 200, 200);
+      background-color: transparent;
+      resize: none;
+      height: 8rem;
+    }
+    & button {
+      width: max-content;
+      margin-top: 2rem;
+      padding: 1rem 2rem;
+      border: 2px solid rgb(144, 148, 152);
+      background-color: transparent;
+    }
+  }
+  .contact-info {
+    color: rgb(113, 121, 129);
+    font-size: 0.8rem;
+    width: 100%;
+    height: 100%;
+    padding: 0 !important;
+    & > div {
+      width: 100%;
+      padding: 4rem;
+      box-sizing: border-box;
+      border: 1px solid rgb(200, 200, 200);
+      flex-grow: 1;
+    }
+  }
+  footer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 2rem;
+    box-sizing: border-box;
+    font-size: 0.8rem;
+    color: white;
+    opacity: 0.5;
   }
 </style>
